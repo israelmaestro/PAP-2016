9c983dbd5e2af4c57c2b79180c2c36d3fce1b343
Israel Oliveira Santos <israelmaestro@gmail.com>
2016-06-17 16:32:15 -0300

    Corrigindo páginas; Corrigindo regras de negócio;

--- Shepherd/src/main/java/br/com/shepherd/service/util/PessoaUtils.java
+++ Shepherd/src/main/java/br/com/shepherd/service/util/PessoaUtils.java
@@ -1,5 +1,8 @@
 package br.com.shepherd.service.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.shepherd.entity.Email;
 import br.com.shepherd.entity.Pessoa;
 import br.com.shepherd.entity.Telefone;
@@ -16,37 +19,31 @@ public class PessoaUtils{
 	 * @return Pessoa
 	 */
 	public Pessoa consistir(Pessoa pPessoa) throws Exception{
-		if(null != pPessoa.getNome() || !pPessoa.getNome().equals("")){
+		if(!pPessoa.getNome().isEmpty()){
 			pPessoa.setNome(pPessoa.getNome().trim());
 		} else{
 			throw new Exception("Nome: Campo obrigatório!");
 		}
 
-		if(null != pPessoa.getSobrenome() || !pPessoa.getSobrenome().equals("")){
+		if(!pPessoa.getSobrenome().isEmpty()){
 			pPessoa.setSobrenome(pPessoa.getSobrenome().trim());
 		} else{
 			throw new Exception("Sobrenome: Campo obrigatório!");
 		}
 
-		if(null != pPessoa.getRg() || !pPessoa.getRg().equals("")){
+		if(!pPessoa.getRg().isEmpty()){
 			pPessoa.setRg(pPessoa.getRg().trim());
 		} else{
 			pPessoa.setRg(null);
 		}
 
-		if(null != pPessoa.getCpf() || !pPessoa.getCpf().equals("")){
+		if(!pPessoa.getCpf().isEmpty()){
 			pPessoa.setCpf(pPessoa.getCpf().trim());
 		} else{
 			pPessoa.setCpf(null);
 		}
 
-		if(pPessoa.isBatizada()){
-			if(null != pPessoa.getDataBatismo() || pPessoa.getDataBatismo().equals("")){
-				pPessoa.setDataBatismo(null);
-			}
-		}
-
-		if(null != pPessoa.getComentarios() || !pPessoa.getComentarios().equals("")){
+		if(!pPessoa.getComentarios().isEmpty()){
 			pPessoa.setComentarios(pPessoa.getComentarios().trim());
 		} else{
 			pPessoa.setComentarios(null);
@@ -60,12 +57,20 @@ public class PessoaUtils{
 		}
 
 		// Consistir lista de Emails
+		List<Email> tEmails = new ArrayList<Email>();
 		for(Email pEmail : pPessoa.getEmails()){
 			pEmail = emailUtils.consistir(pEmail);
 
 			if(emailUtils.isEmpty(pEmail)){
 				pPessoa.getEmails().remove(pEmail);
+			} else{
+				tEmails.add(pEmail);
 			}
+		}
+
+		if(!tEmails.isEmpty()){
+			pPessoa.setEmails(tEmails);
+			tEmails = new ArrayList<Email>();
 		}
 
 		if(pPessoa.getEmails().isEmpty()){
@@ -73,12 +78,21 @@ public class PessoaUtils{
 		}
 
 		// Consistir lista de Telefones
+		List<Telefone> tTelefones = new ArrayList<Telefone>();
+
 		for(Telefone pTelefone : pPessoa.getTelefones()){
 			pTelefone = telefoneUtils.consistir(pTelefone);
 
 			if(telefoneUtils.isEmpty(pTelefone)){
 				pPessoa.getTelefones().remove(pTelefone);
+			} else{
+				tTelefones.add(pTelefone);
 			}
+		}
+
+		if(!tTelefones.isEmpty()){
+			pPessoa.setTelefones(tTelefones);
+			tTelefones = new ArrayList<Telefone>();
 		}
 
 		if(pPessoa.getTelefones().isEmpty()){
